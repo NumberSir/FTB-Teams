a907a12597a6fcec40cf37f2989ef60bb791ec40
Des Herriott <des.herriott@gmail.com>
2026-03-24 12:16:26 +0000

    chore: a few small updates

--- fabric/src/main/java/dev/ftb/mods/ftbteams/api/fabric/FTBTeamsEvents.java
+++ fabric/src/main/java/dev/ftb/mods/ftbteams/api/fabric/FTBTeamsEvents.java
@@ -5,98 +5,98 @@ import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 
 public class FTBTeamsEvents {
-    public static Event<CollectTeamPropertiesEvent> COLLECT_TEAM_PROPERTIES = EventFactory.createArrayBacked(CollectTeamPropertiesEvent.class,
+    public static final Event<CollectTeamPropertiesEvent> COLLECT_TEAM_PROPERTIES = EventFactory.createArrayBacked(CollectTeamPropertiesEvent.class,
             callbacks -> data -> {
                 for (CollectTeamPropertiesEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamCreatedEvent> TEAM_CREATED = EventFactory.createArrayBacked(TeamCreatedEvent.class,
+    public static final Event<TeamCreatedEvent> TEAM_CREATED = EventFactory.createArrayBacked(TeamCreatedEvent.class,
             callbacks -> data -> {
                 for (TeamCreatedEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamDeletedEvent> TEAM_DELETED = EventFactory.createArrayBacked(TeamDeletedEvent.class,
+    public static final Event<TeamDeletedEvent> TEAM_DELETED = EventFactory.createArrayBacked(TeamDeletedEvent.class,
             callbacks -> data -> {
                 for (TeamDeletedEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamLoadedEvent> TEAM_LOADED = EventFactory.createArrayBacked(TeamLoadedEvent.class,
+    public static final Event<TeamLoadedEvent> TEAM_LOADED = EventFactory.createArrayBacked(TeamLoadedEvent.class,
             callbacks -> data -> {
                 for (TeamLoadedEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamSavedEvent> TEAM_SAVED = EventFactory.createArrayBacked(TeamSavedEvent.class,
+    public static final Event<TeamSavedEvent> TEAM_SAVED = EventFactory.createArrayBacked(TeamSavedEvent.class,
             callbacks -> data -> {
                 for (TeamSavedEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamInfoEvent> TEAM_INFO = EventFactory.createArrayBacked(TeamInfoEvent.class,
+    public static final Event<TeamInfoEvent> TEAM_INFO = EventFactory.createArrayBacked(TeamInfoEvent.class,
             callbacks -> data -> {
                 for (TeamInfoEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamPlayerLoggedInEvent> TEAM_PLAYER_LOGGED_IN = EventFactory.createArrayBacked(TeamPlayerLoggedInEvent.class,
+    public static final Event<TeamPlayerLoggedInEvent> TEAM_PLAYER_LOGGED_IN = EventFactory.createArrayBacked(TeamPlayerLoggedInEvent.class,
             callbacks -> data -> {
                 for (TeamPlayerLoggedInEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<PlayerTransferredOwnershipEvent> PLAYER_TRANSFERRED_OWNERSHIP = EventFactory.createArrayBacked(PlayerTransferredOwnershipEvent.class,
+    public static final Event<PlayerTransferredOwnershipEvent> PLAYER_TRANSFERRED_OWNERSHIP = EventFactory.createArrayBacked(PlayerTransferredOwnershipEvent.class,
             callbacks -> data -> {
                 for (PlayerTransferredOwnershipEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<PlayerJoinedPartyTeamEvent> PLAYER_JOINED_PARTY_TEAM = EventFactory.createArrayBacked(PlayerJoinedPartyTeamEvent.class,
+    public static final Event<PlayerJoinedPartyTeamEvent> PLAYER_JOINED_PARTY_TEAM = EventFactory.createArrayBacked(PlayerJoinedPartyTeamEvent.class,
             callbacks -> data -> {
                 for (PlayerJoinedPartyTeamEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<PlayerLeftPartyTeamEvent> PLAYER_LEFT_PARTY_TEAM = EventFactory.createArrayBacked(PlayerLeftPartyTeamEvent.class,
+    public static final Event<PlayerLeftPartyTeamEvent> PLAYER_LEFT_PARTY_TEAM = EventFactory.createArrayBacked(PlayerLeftPartyTeamEvent.class,
             callbacks -> data -> {
                 for (PlayerLeftPartyTeamEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<PlayerChangedTeamEvent> PLAYER_CHANGED_TEAM = EventFactory.createArrayBacked(PlayerChangedTeamEvent.class,
+    public static final Event<PlayerChangedTeamEvent> PLAYER_CHANGED_TEAM = EventFactory.createArrayBacked(PlayerChangedTeamEvent.class,
             callbacks -> data -> {
                 for (PlayerChangedTeamEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamAllyEvent> TEAM_ALLY = EventFactory.createArrayBacked(TeamAllyEvent.class,
+    public static final Event<TeamAllyEvent> TEAM_ALLY = EventFactory.createArrayBacked(TeamAllyEvent.class,
             callbacks -> data -> {
                 for (TeamAllyEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamPropertiesChangedEvent> TEAM_PROPERTIES_CHANGED = EventFactory.createArrayBacked(TeamPropertiesChangedEvent.class,
+    public static final Event<TeamPropertiesChangedEvent> TEAM_PROPERTIES_CHANGED = EventFactory.createArrayBacked(TeamPropertiesChangedEvent.class,
             callbacks -> data -> {
                 for (TeamPropertiesChangedEvent c : callbacks) {
                     c.accept(data);
                 }
             }
     );
-    public static Event<TeamManagerEvent> TEAM_MANAGER = EventFactory.createArrayBacked(TeamManagerEvent.class,
+    public static final Event<TeamManagerEvent> TEAM_MANAGER = EventFactory.createArrayBacked(TeamManagerEvent.class,
             callbacks -> data -> {
                 for (TeamManagerEvent c : callbacks) {
                     c.accept(data);
